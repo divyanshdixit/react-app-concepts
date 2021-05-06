91e6cc287b6fdf0f7b90c204bbbad835e59f8ad7
divyanshdixitmds <divyansh@metadesignsolutions.co.uk>
2021-05-06 23:03:46 +0530

    use ref hook file updated

--- src/components/UseRefHook.js
+++ src/components/UseRefHook.js
@@ -1,15 +1,26 @@
 import React, {useRef, useEffect} from 'react'
 
+// get the dom nodes directly in component: 
+// ref can also be used to store any mutable value:
+
 const UseRefHook = () => {
 
     const inputRef = useRef(null);
+    const paraRef = useRef();
+    const thirdRef= useRef();
 
     useEffect(() => {
+        thirdRef.current = '100';
+        console.log(paraRef.current);
+        console.log(thirdRef.current);
         inputRef.current.focus()
     }, [])
+
     return (
         <div>
-            <input type="text" ref={inputRef}/>
+        <button type="button">button </button>
+        <p ref={paraRef}> this is para </p>
+        <input type="text" ref={inputRef} newProp ={thirdRef.current}/>
         </div>
     )
 }
